6c87753773e224b0ed11221e8e10e77b5e7f371c
sbal13 <sbal13@gmail.com>
2018-04-17 14:04:18 -0400

    ready for stress testing

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
 //Redis
 const redis = require('redis')
+const WebSocket = require('ws')
 const redisConn = redis.createClient();
 
 //WSS
 
+
 function writeTile(obj) {
-  redisConn.set(`${obj.x}-${obj.y}`, obj.color)
+  redisConn.set(`${obj.x}-${obj.y}`, obj.c)
 }
 
 function writeBoard(arr) {
-  arr.forEach(tile => {
-    writeTile(tile)
+  arr.forEach((row, x) => {
+    row.forEach((color, y) => {
+      writeTile({x, y, c: color})
+    })
   })
 }
 
+const dispatch = {
+  "setBoard": writeBoard,
+  "setTile": writeTile,
+}
+
 function handleMsg(msg) {
-  const action = msg.data.action
-  const action = msg.data.payload
+  const {action, payload} = JSON.parse(msg.data)
+  console.log(action, payload)
   dispatch[action](payload)
 }
 
@@ -24,10 +33,6 @@ function requestBoard(ws) {
   ws.send(JSON.stringify({action: "getBoard", payload: null}))
 }
 
-const dispatch = {
-  "board": writeBoard,
-  "tile": writeTile,
-}
 
 const wsConn = new WebSocket("ws://localhost:8080")
 wsConn.addEventListener('open', () => requestBoard(wsConn))
