2545155197598f2b42b9e0e7d35de84c86de9950
sbal13 <sbal13@gmail.com>
2018-04-21 10:03:04 -0400

    Added final socket url

--- server.js
+++ server.js
@@ -4,37 +4,35 @@ const WebSocket = require('ws')
 const redisConn = redis.createClient();
 
 //WSS
-
-
 function writeTile(obj) {
-  redisConn.set(`${obj.x}-${obj.y}`, obj.c)
+	redisConn.set(`${obj.x}-${obj.y}`, obj.c)
 }
 
 function writeBoard(arr) {
-  arr.forEach((row, x) => {
-    row.forEach((color, y) => {
-      writeTile({x, y, c: color})
-    })
-  })
+	arr.forEach((row, x) => {
+		row.forEach((color, y) => {
+			writeTile({x, y, c: color})
+		})
+	})
 }
 
 const dispatch = {
-  "setBoard": writeBoard,
-  "setTile": writeTile,
+	"setBoard": writeBoard,
+	"setTile": writeTile,
 }
 
 function handleMsg(msg) {
-  const {action, payload} = JSON.parse(msg.data)
-  console.log(action, payload)
-  dispatch[action](payload)
+	const {action, payload} = JSON.parse(msg.data)
+	console.log(action, payload)
+	dispatch[action](payload)
 }
 
 function requestBoard(ws) {
-  ws.send(JSON.stringify({action: "getBoard", payload: null}))
+	ws.send(JSON.stringify({action: "getBoard", payload: null}))
 }
 
 
-const wsConn = new WebSocket("ws://localhost:8080")
+const wsConn = new WebSocket("ws://mayisgr8.win:8080")
 wsConn.addEventListener('open', () => requestBoard(wsConn))
 wsConn.addEventListener('message', (msg) => handleMsg(msg))
 wsConn.addEventListener('close', () => console.log('ws closed'))
